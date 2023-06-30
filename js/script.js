3c709d76bd825781c256e90dda8377d9a58efa53
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-06-30 18:36:32 +0200

    add img end bonus

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ createApp({
   data() {
     return {
       message: 'Hello Vue!',
-      styleClass: 'text-center text-uppercase text-success my-5'
+      styleClass: 'text-center text-uppercase text-success my-5',
+      randomImg: `https://unsplash.it/300/300?image=${Math.floor(Math.random()* 3)}`
     }
   }
 }).mount('#app')
